165133be271ebe4987a2bcaa3350302d0870c8b8
MeMo <103096364+0MeMo07@users.noreply.github.com>
2024-03-06 14:38:01 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './css/App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
+import axios from 'axios'
 function App() {
   const [tasks, setTasks] = useState([])
-  const TaskValues = (title, TaskDec) => {
-    const TaskContainer = [
-      ...tasks, {
-        id:Math.round(Math.random() * 99999),
-        title,
-        TaskDec
-      }
-    ]
+
+  const TaskValues = async (title, TaskDec) => {
+    const response =await axios.post("http://localhost:3000/tasks", {
+      title,
+      TaskDec
+    })
+    const TaskContainer = [...tasks, response.data]
     setTasks(TaskContainer)
   }
-  const DeleteTaskById = (id) => {
+
+  const fetchTasks = async()=>{
+  const response = await axios.get("http://localhost:3000/tasks")
+  setTasks(response.data)
+  }
+  useEffect(() => {
+    fetchTasks()
+  },[])
+
+  const DeleteTaskById = async(id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`)
     const afterDeleteTask = tasks.filter((task) => {
       return task.id !== id;
     })
     setTasks(afterDeleteTask)
   }
 
-  const EditTaskById = (id, updateTitle, updateDec) => {
+  const EditTaskById = async(id, updateTitle, updateDec) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`, {
+      title: updateTitle,
+      TaskDec: updateDec
+    })
     const UpdateTask = tasks.map((task) => {
       if (task.id === id) {
         return {
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/react-task-app">
   <App />
+  </BrowserRouter>
 )
